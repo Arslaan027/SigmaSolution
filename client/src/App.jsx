144cace4ac72586404cbacc15637b618e860a76b
Arslaan Ahmad <arslaanahmadrajput@gmail.com>
2024-09-02 23:09:44 +0530

    New Comp Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./Components/Footer/Footer";
 import ComparisonSection from "./Components/Home/ComparisonSection";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import Experts from "./Components/Home/Experts";
 
 const App = () => {
   return (
@@ -19,8 +19,8 @@ const App = () => {
       <WhyLegalWiz />
       <Trust />
       <ComparisonSection />
+      <Experts />
       <Footer />
-
     </div>
   );
 };
