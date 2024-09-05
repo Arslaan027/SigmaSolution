ab61a7f65afb6557368ab5628e2992e0cf834901
Arslaan Ahmad <arslaanactive@gmail.com>
2024-09-06 01:32:11 +0530

    new items

--- client/src/Components/HoReCa/Products.jsx
+++ client/src/Components/HoReCa/Products.jsx
@@ -15,7 +15,7 @@ const Products = () => {
       description: "Premium Arabica coffee beans.",
       icon: <FiCoffee className="text-red-500 text-2xl mr-3" />,
       imageUrl:
-        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE    ",
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Tea Bags",
@@ -49,152 +49,177 @@ const Products = () => {
       title: "Dairy Products",
       description: "Fresh milk and cream.",
       icon: <FiTruck className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Frozen Meats",
       description: "Chicken, beef, lamb.",
       icon: <FiShoppingBag className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Cleaning Supplies",
       description: "Industrial cleaning chemicals.",
       icon: <FiCheckSquare className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Cooking Oils",
       description: "Olive oil, sunflower oil.",
       icon: <FiPackage className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Paper Products",
       description: "Napkins, towels, tissues.",
       icon: <FiBox className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Spices & Seasonings",
       description: "Chili, oregano, salt.",
       icon: <FiCheckSquare className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Canned Foods",
       description: "Tomatoes, beans, corn.",
       icon: <FiPackage className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Non-Alcoholic Beverages",
       description: "Juices, soda, water.",
       icon: <FiTruck className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Pasta & Grains",
       description: "Pasta, rice, quinoa.",
       icon: <FiCoffee className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Sugar & Sweeteners",
       description: "White and brown sugar.",
       icon: <FiShoppingBag className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Disposable Cutlery",
       description: "Plastic forks, spoons, knives.",
       icon: <FiCheckSquare className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Packaging Materials",
       description: "Food-grade containers.",
       icon: <FiPackage className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Sauces & Condiments",
       description: "Ketchup, mustard, sauces.",
       icon: <FiBox className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Bakery Products",
       description: "Bread, cakes, pastries.",
       icon: <FiShoppingBag className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Frozen Desserts",
       description: "Ice cream, sorbets.",
       icon: <FiPackage className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Frozen Seafood",
       description: "Shrimp, salmon, tuna.",
       icon: <FiTruck className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Meat Substitutes",
       description: "Tofu, tempeh, soy-based products.",
       icon: <FiCheckSquare className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Wine & Spirits",
       description: "Red wine, vodka, whiskey.",
       icon: <FiBox className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Bottled Water",
       description: "Still and sparkling water.",
       icon: <FiTruck className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Snacks & Chips",
       description: "Potato chips, nuts, crackers.",
       icon: <FiCoffee className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Frozen Dough",
       description: "Pre-made dough for pizza, bread.",
       icon: <FiShoppingBag className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Cheese Varieties",
       description: "Cheddar, mozzarella, brie.",
       icon: <FiPackage className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Cooking Utensils",
       description: "Knives, spatulas, tongs.",
       icon: <FiCheckSquare className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Frozen Fruits",
       description: "Frozen berries, mangoes.",
       icon: <FiBox className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
     {
       title: "Organic Produce",
       description: "Fresh organic vegetables and fruits.",
       icon: <FiTruck className="text-red-500 text-2xl mr-3" />,
       icon: <FiBox className="text-red-500 text-2xl mr-3" />,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxPeUp-FeNpODI9a9bIkTDduBgzW5aUaNJaqEOafp21el_uugZvhZlvLbFe68DxhcoX4mW4LxiEgFQTDOArPX_qiWu8leRa3fZxjdac2PV&usqp=CAE",
     },
   ];
 
@@ -213,13 +238,13 @@ const Products = () => {
         {products.map((product, index) => (
           <div
             key={index}
-            className="bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out flex flex-col"
+            className="bg-white border border-gray-200 rounded-3xl shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out flex flex-col"
           >
-            <div className="relative flex-shrink-0 h-48">
+            <div className="relative flex-shrink-0 h-56 mb-8">
               <img
                 src={product.imageUrl}
                 alt={product.title}
-                className="w-full h-full object-cover rounded-t-lg"
+                className="w-full h-full object-cover rounded-t-3xl"
               />
               <div className="absolute top-4 right-4 p-2 bg-white rounded-lg shadow-lg flex items-center">
                 {product.icon}
